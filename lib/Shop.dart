e0d42d48b7794f9787acc8781d1041f393f84f89
Draeco <luisapontecepeda@gmail.com>
2021-12-10 21:45:44 -0500

    inclusion Shop

---
+++ lib/Shop.dart
@@ -0,0 +1,57 @@
+import 'package:flutter/material.dart';
+
+class Shop extends StatefulWidget{
+  @override
+  ShopApp createState()=> ShopApp();
+}
+
+class ShopApp extends State<Shop>{
+  Widget build(BuildContext context){
+    Widget titleSection = Container(
+      padding: const EdgeInsets.all(32),
+      child: Row(
+        children:[
+          Expanded(child:
+          Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children:[
+              Container(
+                padding: const EdgeInsets.only(bottom: 8),
+                child: Text(
+                  "comidas rapidas el gordo",
+                  style: TextStyle(
+                    fontWeight: FontWeight.bold,
+                  ),
+                ),
+              ),
+              Text( "Perros calientes, Hamburguesas y más...",
+                  style: TextStyle(
+                    color: Colors.greenAccent[500],
+                  ),
+              ),
+
+            ],
+          ),
+          ),
+          Container(
+            width: 80,
+            height: 80,
+            child: Image.asset("image/gordo.png"),
+          ),
+          ElevatedButton(onPressed: (){}, child:Text("entrar"))
+        ],
+      ),
+    );
+    return Scaffold(
+      appBar: AppBar(
+        title:Text("Lista de Tiendas"),
+      ),
+      body: ListView(
+        children: [
+          titleSection,
+          titleSection
+        ],
+      ),
+    );
+  }
+}
--- lib/main.dart
+++ lib/main.dart
@@ -2,7 +2,7 @@ import "package:flutter/material.dart";
 import "package:cloud_firestore/cloud_firestore.dart";
 import "package:firebase_core/firebase_core.dart";
 import "buscar.dart";
-
+import "Shop.dart";
 void main() {
   WidgetsFlutterBinding.ensureInitialized();
   Firebase.initializeApp();
@@ -61,8 +61,20 @@ class HomeStart extends State<Home> {
                 },
                 child: Text("Buscar"),
               ),
-            )
-          ]),
+            ),
+            Padding(
+                padding: EdgeInsets.only(left: 20,top: 20,right: 20,bottom: 0),
+                child: ElevatedButton(
+                  onPressed: (){
+                    Navigator.push(
+                        context, MaterialPageRoute(builder: (_) => Shop()));
+                  },
+                  child: Text("Listado de Tiendas"),
+                ),
+
+            ),
+          ],
+          ),
         ),
       ),
     );
